442ebdbd8a6485a3889d1d58b46288d45863fca3
Diogo Santos <diogodeese@gmail.com>
2021-10-25 04:53:07 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+  const [app, setApp] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=G1B8m5s7pM0Y0Y7l7zMuDGC4UzvF1u9ldcFAhVSt"
+      )
+      .then((res) => {
+        setApp(res.data);
+      });
+  });
+
+  if (app) {
+    return (
+      <div className="App">
+        <h1>{app.date}</h1>
+        <h4>{app.explanation}</h4>
+        <h1>{app.hdurl}</h1>
+        <h1>{app.media_type}</h1>
+        <h1>{app.service_version}</h1>
+        <h1>{app.title}</h1>
+        <h1>{app.url}</h1>
+        <img src={app.url} alt="Logo" />
+      </div>
+    );
+  }
+  return <div className="App"></div>;
 }
-
-export default App;
